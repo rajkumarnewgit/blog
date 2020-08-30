d6eec7a270abfba7e08f6aa17285ae929650d868
rajkumarnewgit <rajkumarnewgit>
2020-08-30 22:18:59 +0530

    blog

---
+++ posts/serializers.py
@@ -0,0 +1,19 @@
+from .models import Posts
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Posts
+        fields = ('title', 'content', 'if_featured')
+
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields =  '__all__'
